c91af5e94a0c2c86113971a9776359f8ecc6be7c
Filip Anic <superfilles12@gmail.com>
2021-01-06 21:29:09 +0100

    new stuff

---
+++ Assets/MovingTarget.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTarget : MonoBehaviour
+{
+    public float speed = 1;
+    public Vector3 target = Vector3.zero;
+
+    private Vector3 currentTarget = Vector3.zero;
+    private Vector3 startingPosition = Vector3.zero;
+    private string direction = "towardsTarget";
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+        currentTarget = target;
+
+        if (Vector3.Distance(transform.position, target) <= 0.001f)
+        {
+            currentTarget = startingPosition;
+            direction = "towardsStartingPosition";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction == "towardsTarget")
+        {
+            if (Vector3.Distance(transform.position, target) <= 0.001f)
+            {
+                currentTarget = startingPosition;
+                direction = "towardsStartingPosition";
+            }
+        } else
+        {
+            if (Vector3.Distance(transform.position, startingPosition) <= 0.001f)
+            {
+                currentTarget = target;
+                direction = "towardsTarget";
+            }
+        }
+
+        //Vector3.RotateTowards(transform.position, currentTarget)
+
+        float step = Time.deltaTime * speed;
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, step);
+    }
+}
